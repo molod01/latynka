45300e210730864f7a96bcbe187b6709dc76d708
molod01 <manuylovnikitos@gmail.com>
2022-12-21 00:47:21 +0200

    start

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ import bodyParser from 'body-parser';
 import ejsMate from 'ejs-mate';
 import express from 'express';
 import path from 'path';
-import config from './config/conf.js';
 import router from './routes/routes.js';
+const PORT = process.env.PORT ? process.env.PORT : 5000;
 
 const app = express();
 const __dirname = path.resolve();
 app.set('view engine', 'ejs');
 app.engine('ejs', ejsMate);
-app.set('port', config.PORT);
+app.set('port', PORT);
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(
@@ -19,4 +19,4 @@ app.use(
 );
 app.use(router);
 app.use(express.static(path.resolve(__dirname, 'views')));
-app.listen(config.PORT, console.log(`http://localhost:${config.PORT}`));
+app.listen(config.PORT, console.log(`port:${PORT}`));
